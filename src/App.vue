2b21eeab1fd593849dfc241eccf8ad11f09e6d02
Diego Henrique Oliveira <diegoholiveirav@gmail.com>
2021-02-19 21:58:47 -0300

    Poke Vue v1.0

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="pokedex">
+    <Pokedex />
+  </div>
+</template>
+
+<script>
+import Pokedex from "./components/Pokedex.vue";
+
+export default {
+  name: "App",
+  components: {
+    Pokedex,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#pokedex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh);
+  padding: 20px;
+
+  background: url("./assets/florest.jpg");
+  background-size: 100% 100%;
+}
+</style>
